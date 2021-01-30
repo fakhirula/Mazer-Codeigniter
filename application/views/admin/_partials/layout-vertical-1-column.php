751186322831b3b4314b625fbef3f19b4a8423aa
fakhirula <inyour.nase@gmail.com>
2021-01-30 20:34:47 +0700

    fixed bug

--- application/views/admin/_partials/layout-vertical-1-column.php
+++ application/views/admin/_partials/layout-vertical-1-column.php
@@ -9,41 +9,28 @@
     <link rel="stylesheet" href="<?php echo base_url('') ?>assets/css/app.css">
 
 </head>
-<body>
-    <div id="app">
-        <?php $this->load->view("admin/_partials/sidebar.php") ?>
-        <div id="main">
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
-            </header>
-            
-            <nav class="navbar navbar-light">
-                <div class="container d-block">
-                    <a href="index.html"><i class="bi bi-chevron-left"></i></a>
-                    <a class="navbar-brand ms-4" href="index.html">
-                        <img src="<?php echo base_url('') ?>assets/images/logo/logo.png">
-                    </a>
-                </div>
-            </nav>
-            
-            
-            <div class="container">
-                <div class="card mt-5">
-                    <div class="card-header">
-                        <h4 class="card-title">Single Layout</h4>
-                    </div>
-                    <div class="card-body">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo quasi labore expedita?
-                            Veritatis
-                            at maxime id voluptates excepturi molestiae possimus blanditiis dicta consequuntur maiores suscipit,
-                            eveniet neque obcaecati doloribus.</p>
-                    </div>
-                </div>
-            </div>
+<body>       
+    <nav class="navbar navbar-light">
+        <div class="container d-block">
+            <a href="<?php echo base_url('')?>admin"><i class="bi bi-chevron-left"></i></a>
+            <a class="navbar-brand ms-4" href="<?php echo base_url('')?>admin">
+                <img src="<?php echo base_url('') ?>assets/images/logo/logo.png">
+            </a>
+        </div>
+    </nav>
 
-            <?php $this->load->view("admin/_partials/footer.php") ?>
+
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-header">
+                <h4 class="card-title">Single Layout</h4>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo quasi labore expedita?
+                    Veritatis
+                    at maxime id voluptates excepturi molestiae possimus blanditiis dicta consequuntur maiores suscipit,
+                    eveniet neque obcaecati doloribus.</p>
+            </div>
         </div>
     </div>
     <?php $this->load->view("admin/_partials/btm_script.php") ?>
